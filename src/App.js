a7a448efc19b66946eda49f6abd57854627cd4a6
Ashar Ali <ashar.alia1999@gmail.com>
2021-03-26 20:30:05 +0500

    Done making Coffee cards

--- src/App.js
+++ src/App.js
@@ -1,27 +1,21 @@
-import { Grid } from "@material-ui/core";
+import {Grid } from "@material-ui/core";
 import "./styles.css";
-
+import Header from "./Components/Header/Header";
+import CardSection from "./Components/CardSection/CardSection"
+import CoffeeCard from "./Components/CoffeeCard/CoffeeCard"
 export default function App() {
   return (
-   <Grid container direction = "column">
-    <Grid item container>
-      <Grid item xs={0} sm={2}/>
-      <Grid item sm={8} xs={12} >
-        This is material UI
-        This is material UI
-        This is material UI
-        This is material UI
-        This is material UI
-        This is material UI
-        This is material UI
-        This is material UI
-        This is material UI
-        This is material UI
-        This is material UI
-        This is material UI
-        </Grid>
-      <Grid item xs={0} sm={2}/>
+    <Grid container direction="column">
+    <Grid item>
+      <Header />
     </Grid>
-   </Grid>
+      <Grid item container>
+        <Grid item xs={0} sm={2}/>
+        <Grid item xs={12} sm={8}>
+          <CardSection/>
+        </Grid>
+        <Grid item xs={0} sm={2}/>
+      </Grid>
+    </Grid>
   );
 }
